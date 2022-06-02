f66d7fd79dea0f08e9ceaf2e77972c7eedde1361
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-06-02 17:59:19 +0530

    slug updated

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,41 @@
 import React from "react";
+import { client } from "../../lib/client";
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
   return <div>ProductDetails</div>;
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+      slug {
+        current
+      }
+    }
+    `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+  return {
+    props: { products, product },
+  };
 };
 
 export default ProductDetails;
