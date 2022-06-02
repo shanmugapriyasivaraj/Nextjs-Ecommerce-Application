18a952800a6d04f8d245dfdfb0994c05eedfb77b
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-06-02 19:42:41 +0530

    Product page updated

--- components/Product.js
+++ components/Product.js
@@ -5,7 +5,7 @@ import { urlfor } from "../lib/client";
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
-      <Link href={`product/${slug.current}`}>
+      <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img
             src={urlfor(image && image[0])}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,86 @@
-import React from "react";
-import { client } from "../../lib/client";
+import React, { useState } from "react";
+import { client, urlfor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+import Product from "../../components/Product";
 
 const ProductDetails = ({ product, products }) => {
-  return <div>ProductDetails</div>;
+  const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img
+              src={urlfor(image && image[index])}
+              alt=""
+              className="product-detail-image"
+            />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlfor(item)}
+                alt=""
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>{20}</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">₹{price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">1</span>
+              <span className="plus">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button className="add-to-cart">Add to Cart</button>
+            <button className="buy-now">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const getStaticPaths = async () => {
