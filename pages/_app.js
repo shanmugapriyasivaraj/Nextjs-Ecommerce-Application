59d64a3898d1431d34653f8f8d95d10cf6447e4e
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-06-02 20:19:32 +0530

    State Context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Layout from "../components/Layout";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Toaster />
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
