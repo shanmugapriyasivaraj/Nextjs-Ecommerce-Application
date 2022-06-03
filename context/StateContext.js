dae7c6b0c33676f53173ac50006fc6c691b39d28
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-06-03 07:42:26 +0530

    commit

--- context/StateContext.js
+++ context/StateContext.js
@@ -57,7 +57,6 @@ export const StateContext = ({ children }) => {
 
   const toggleCartItemQuanitity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id);
-    index = cartItems.findIndex((product) => product._id === id);
     const newCartItems = cartItems.filter((item) => item._id !== id);
 
     if (value === "inc") {
